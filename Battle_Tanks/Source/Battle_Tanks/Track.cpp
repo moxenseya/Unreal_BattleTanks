bee9095b12d9fbdf786dedd6492d435a10686d07
N-GEN\Roxas-76927 <keyblademaster1000@gmail.com>
2018-01-17 21:12:31 +0530

    Track Throttle input mapped to C++ Track class method

---
+++ Battle_Tanks/Source/Battle_Tanks/Track.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Track.h"
+
+
+
+void UTrack::SetThrottle(float Throttle)
+{
+
+	UE_LOG(LogTemp, Warning, TEXT("Throttling!! %f") ,Throttle);
+}
---
+++ Battle_Tanks/Source/Battle_Tanks/Track.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "Track.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLE_TANKS_API UTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void SetThrottle(float Throttle);
+	
+	
+};
